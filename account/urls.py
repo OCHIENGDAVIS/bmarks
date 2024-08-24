6dcf3a7a5016629cbe7097c2b4edad96f6358798
davis <davisochieng4@gmail.com>
2024-08-24 09:38:20 +0300

    Add password change and reser - this change adds the ability to change and reset password using the django authentication system

--- account/urls.py
+++ account/urls.py
@@ -3,16 +3,22 @@ from django.contrib.auth import views as auth_views
 
 from . import views
 
-
-app_name='account'
+app_name = 'account'
 
 urlpatterns = [
     # login / logout urls 
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
-    
+
     # change password urls 
     path('password-change/', views.CustomPasswordChangeView.as_view(), name='password_change'),
     path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
+    # reset password urls
+    path('password-reset/', views.CustomPasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>/', views.CustomPasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
--- account/views.py
+++ account/views.py
@@ -2,11 +2,10 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.contrib.auth.views import PasswordChangeView
+from django.contrib.auth.views import PasswordChangeView, PasswordResetView, PasswordResetConfirmView
 from django.urls import reverse_lazy
 
 from .forms import LoginForm
-
 
 
 def user_login(request):
@@ -29,12 +28,23 @@ def user_login(request):
     return render(request, 'account/login.html', {'form': form})
 
 
-
 @login_required
 def dashboard(request):
     return render(request, 'account/dashboard.html', {'section': 'dashboard'})
 
 
-
 class CustomPasswordChangeView(PasswordChangeView):
     success_url = reverse_lazy('account:password_change_done')
+
+
+class CustomPasswordResetView(PasswordResetView):
+    success_url = reverse_lazy('account:password_reset_done')
+
+
+class CustomPasswordResetConfirmView(PasswordResetConfirmView):
+    success_url = reverse_lazy('account:password_reset_confirm')
+
+    def get_success_url(self):
+        uidb64 = self.kwargs['uidb64']
+        token = self.kwargs['token']
+        return reverse_lazy('account:password_reset_confirm', kwargs={'uidb64': uidb64, 'token': token})
