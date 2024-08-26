8c1c48e48bd07b37fa1a856ff403efd8dd8b246c
davis <davisochieng4@gmail.com>
2024-08-26 13:20:20 +0300

    loading images using ajax - this change adds the ability to load images using ajax in a infinite scroll of the images page.

--- images/urls.py
+++ images/urls.py
@@ -6,5 +6,7 @@ app_name = 'images'
 
 urlpatterns = [
     path('create/', views.image_create, name='create'),
-    path('<int:image_id>/<slug:slug>/detail/', views.image_detail, name='detail')
+    path('<int:image_id>/<slug:slug>/detail/', views.image_detail, name='detail'),
+    path('like/', views.image_like, name='like'),
+    path('', views.image_list, name='list'),
 ]
--- images/views.py
+++ images/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.views.decorators.http import require_POST
+from django.http import JsonResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.http import HttpResponse
 
 from .forms import ImageCreateForm
 from .models import Image
@@ -31,3 +35,41 @@ def image_create(request):
 def image_detail(request, image_id, slug):
     img = get_object_or_404(Image, id=image_id, slug=slug)
     return render(request, 'images/detail.html', {'image': img})
+
+
+@require_POST
+@login_required()
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except Image.DoesNotExist:
+            pass
+    return JsonResponse({'status': 'error'})
+
+
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 10)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+    except EmptyPage:
+
+        if images_only:
+            return HttpResponse('')
+        images = paginator.page(paginator.num_pages)
+    if images_only:
+        return render(request, 'images/list_images.html', {'section': 'images', 'images': images})
+    ctx = {'images': images, 'section': 'images'}
+    return render(request, 'images/list.html', context=ctx)
